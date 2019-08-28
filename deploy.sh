58204a8f4dd5637fd36c06dd12b6f52129b25699
nadayasinta <nada@alterra.id>
2019-08-28 15:38:08 +0700

    travis3

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,2 @@
-sudo docker stop flaskecommerce
-sudo docker rm flaskecommerce
-sudo docker rmi nadayasinta/flaskecommerce:latest
+            
 sudo docker run -d --name flaskecommerce -p 5000:5000 nadayasinta/flaskecommerce:latest
